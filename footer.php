754a7ccd96f8c93495d2a9194fb1fcd971511779
Rebecca Schoenrock <rkschoenrock@gmail.com>
2016-05-28 10:52:02 -0400

    Separate Header and Footer from Index.

---
+++ footer.php
@@ -0,0 +1,11 @@
+<footer class="row">
+    <div class="twelve columns">
+        <h2>This is the footer</h2>
+          <p>This is some cool footer content</p>
+    </div>
+</footer>
+
+</div> <!-- End Container Div -->
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Rebecca Schoenrock Blank Template</title>
+
+    <link rel="stylesheet" type="text/css" href="
+        <?php bloginfo('stylesheet_url'); ?>
+    " />
+</head>
+<body>
+    <div class="container"> <!-- Begin Container Div -->
+
+        <header class="row">
+            <div class="twelve columns">
+                <h1>This is My Blog</h1>
+                <p>This is my text</p>
+            </div>
+        </header>
