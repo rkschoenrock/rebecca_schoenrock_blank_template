0ff5ec714eeb2ae0aed9c5ecfca1e18b5bbd5c51
Rebecca Schoenrock <rkschoenrock@gmail.com>
2016-05-28 10:33:25 -0400

    Add skeleton structure to the site.

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Rebecca Schoenrock Blank Template</title>
+
+    <link rel="stylesheet" type="text/css" href="
+        <?php bloginfo('stylesheet_url'); ?>
+    " />
+</head>
+<body>
+    <div class="container">
+        <header class="row">
+            <div class="twelve columns">
+                <h1>This is My Blog</h1>
+                <p>This is my text</p>
+            </div>
+        </header>
+
+        <section class="row">
+            <div class="twelve columns">
+                <h2>Section Content - cool</h2>
+                  <p>This is a cool section</p>
+            </div>
+        </section>
+
+        <footer class="row">
+            <div class="twelve columns">
+                <h2>This is the footer</h2>
+                  <p>This is some cool footer content</p>
+            </div>
+        </footer>
+    </div>
+</body>
+
+</html>
