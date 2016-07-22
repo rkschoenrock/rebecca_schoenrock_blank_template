f083963922a57653a43334b172352b2e0ad5cb58
Rebecca Schoenrock <rkschoenrock@gmail.com>
2016-07-22 11:38:27 -0400

    Create footer

--- footer.php
+++ footer.php
@@ -1,14 +1,16 @@
-<footer class="row">
-    <div class="four columns">
-        <?php dynamic_sidebar('footer-one'); ?>
-</div>
-    <div class="four columns">
-        <?php dynamic_sidebar('footer-two'); ?>
-</div>
-    <div class="four columns">
-        <?php dynamic_sidebar('footer-three'); ?>
-</div>
-</footer>
+<footer>
+    <div class="social">
+        <a href=""><img src="images/facebook-white.png" width="60px"/></a>
+        <a href=""><img src="images/twitter-white.png" width="60px"/></a>
+        <a href=""><img src="images/instagram-white.png" width="60px"/></a>
+        <a href=""><img src="images/pinterest-white.png" width="60px"/></a>
+        <a href=""><img src="images/email-white.png" width="60px"/></a>
+    </div>
+    <h2><a href="index.php">Faith 'Viera' Lutheran Church</a></h2>
+    <p><span class="address">5550 Faith Dr, Rockledge, FL 32955</span> |
+        <span class="address">info@faithviera.org</span> |
+        <span class="address">(321) 636-5504</span>
+    </p>
 </footer>
 
 </div> <!-- End Container Div -->
